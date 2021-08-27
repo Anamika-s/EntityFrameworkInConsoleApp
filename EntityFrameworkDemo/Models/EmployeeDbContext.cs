54ab48b1313e64e96e93667426b0b2fa650e1652
anamika.sawhney22@gmail.com <anamika.sawhney22@gmail.com>
2021-08-27 06:21:06 +0000

    Entity Framework in Console Application Demo

---
+++ EntityFrameworkDemo/Models/EmployeeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproachDemo.Models
+{
+    public class EmployeeDbContext: DbContext // FOr this we need Microsoft.EntityFrameworkCore;
+    {
+        private const string connectionString = "server=admivm\\SQLEXPRESS;database=PracticeDemo;user id=sa;password=pass@123";
+
+
+        public EmployeeDbContext()
+        {
+
+        }
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+                   : base(options) { }
+        
+        public DbSet<Emp> Emps { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder modelBuilder)
+        {
+            modelBuilder.UseSqlServer(connectionString);
+        }
+
+
+        }
+    }
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using CodeFirstApproachDemo.Models;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeeDbContext db = new EmployeeDbContext();
+            // CRUD Operations
+            // LINQ 
+
+            // Get all Records
+            var list = db.Emps.ToList();
+            if(list.Count>0)
+            {
+                foreach (var temp in list)
+                {
+                    Console.WriteLine(temp.Id + " " + temp.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Records found");
+            }
+
+            // Inserting Record
+            db.Emps.Add(new Emp() { Name = "Sagar", Dept = "HR", Salary = 90000 });
+            db.SaveChanges();
+
+            // For a Single record
+            var emp = db.Emps.Where(x => x.Id == 1).FirstOrDefault();
+            if(emp!=null)
+            {
+                Console.WriteLine(emp.Id + " " + emp.Name);
+            }
+            else
+            {
+                Console.WriteLine("No Record Found");
+            }
+
+            // Update Record
+
+              emp = db.Emps.Where(x => x.Id == 1).FirstOrDefault();
+            if (emp != null)
+            {
+                emp.Dept = "Accts";
+                emp.Salary = 30000;
+                db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("No Record Found");
+            }
+
+            // Delete Record
+
+            emp = db.Emps.Where(x => x.Id == 1).FirstOrDefault();
+            if (emp != null)
+            {
+                db.Emps.Remove(emp);
+                db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("No Record Found");
+            }
+
+
+        }
+    }
+}
